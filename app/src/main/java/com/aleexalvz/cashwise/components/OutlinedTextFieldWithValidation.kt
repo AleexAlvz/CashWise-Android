a293610d5b05bf15146745c4b34b27f17bd885b1
Alex Alves <alex.alves_br@hotmail.com>
2023-08-22 18:25:59 -0300

    fix visual transformation for passwords

--- app/src/main/java/com/aleexalvz/cashwise/components/OutlinedTextFieldWithValidation.kt
+++ app/src/main/java/com/aleexalvz/cashwise/components/OutlinedTextFieldWithValidation.kt
@@ -11,6 +11,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import com.aleexalvz.cashwise.ui.theme.Green
 
 @Composable
@@ -38,10 +40,12 @@ fun outlinedTextFieldWithValidation(
     )
 
     val passwordHidden = rememberSaveable { mutableStateOf(true) }
-
+    val visualTransformation =
+        if (passwordHidden.value) PasswordVisualTransformation() else VisualTransformation.None
     OutlinedTextField(
         modifier = modifier,
         value = field.value,
+        visualTransformation = visualTransformation,
         trailingIcon = {
             if (isPassword) {
                 IconButton(onClick = { passwordHidden.value = !passwordHidden.value }) {
