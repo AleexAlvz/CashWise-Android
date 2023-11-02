91305a1ef9245cc6a299eefe6ac1c5d94d638aca
Alex Alves <alex.alves_br@hotmail.com>
2023-11-02 17:34:24 -0300

    clean project unused code

--- app/src/main/java/com/aleexalvz/cashwise/feature/login/LoginActivity.kt
+++ app/src/main/java/com/aleexalvz/cashwise/feature/login/LoginActivity.kt
@@ -2,13 +2,10 @@ package com.aleexalvz.cashwise.feature.login
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import com.aleexalvz.cashwise.feature.home.HomeActivity
 import com.aleexalvz.cashwise.feature.login.ui.LoginNavGraph
-import com.aleexalvz.cashwise.feature.login.viewmodel.SignUpViewModel
 import com.aleexalvz.cashwise.foundation.UserManager
 import com.aleexalvz.cashwise.ui.theme.CashWiseTheme
 import dagger.hilt.android.AndroidEntryPoint
--- app/src/main/java/com/aleexalvz/cashwise/ui/theme/Theme.kt
+++ app/src/main/java/com/aleexalvz/cashwise/ui/theme/Theme.kt
@@ -1,43 +1,23 @@
 package com.aleexalvz.cashwise.ui.theme
 
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
 
-private val DarkColorScheme = darkColorScheme(
+private val darkColorScheme = darkColorScheme(
     primary = GrayDefault,
     secondary = GrayDefault,
     tertiary = Pink80,
     background = DarkBackground
 )
 
-//private val LightColorScheme = lightColorScheme( //TODO configure this
-//    primary = Green,
-//    secondary = Green,
-//    tertiary = Pink40,
-//    background = Color.Blue
-//)
-
 @Composable
 fun CashWiseTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colorScheme = if (darkTheme) DarkColorScheme else DarkColorScheme
-
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
-        }
-    }
+    val colorScheme = darkColorScheme
 
     MaterialTheme(
         colorScheme = colorScheme,
