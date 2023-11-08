0bd501a43216b772e136b31d79a8bb1ec6adf615
Alex Alves <alex.alves_br@hotmail.com>
2023-11-08 20:49:32 -0300

    add background on home

--- app/src/main/java/com/aleexalvz/cashwise/feature/home/home/HomeScreen.kt
+++ app/src/main/java/com/aleexalvz/cashwise/feature/home/home/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,6 +22,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,7 +53,17 @@ fun HomeScreen(
 ) {
 
     if (uiState.isFetchedData.not()) fetchScreenData()
-
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Image(
+            modifier = Modifier.fillMaxWidth(),
+            painter = painterResource(id = R.drawable.home_background_img),
+            contentDescription = "home background",
+            contentScale = ContentScale.FillWidth
+        )
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
