3fac3882633262df7dc819191f10a55d17050cad
Alex Alves <alex.alves_br@hotmail.com>
2023-11-16 21:17:14 -0300

    create tests for helper path

---
+++ app/src/test/java/com/aleexalvz/cashwise/helper/AuthHelperTest.kt
@@ -0,0 +1,69 @@
+package com.aleexalvz.cashwise.helper
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class AuthHelperTest {
+
+    //Testing validateEmail
+    @Test
+    fun `when pass valid email string then return true`() {
+        listOf(
+            "user@example.com",
+            "user123@example.com.br",
+            "user.name@domain.com",
+            "user_name@domain.net"
+        ).forEach { case ->
+            val actual = AuthHelper.validateEmail(case)
+            assertTrue(actual)
+        }
+    }
+
+    @Test
+    fun `when pass invalid email string then return false`() {
+        listOf(
+            "user@.com",
+            "user@com",
+            "user@domain",
+            "user@domain.",
+            "user@domain..com",
+            "user@.domain.com",
+            "@domain.com",
+            "user@domaincom"
+        ).forEach { case ->
+            val actual = AuthHelper.validateEmail(case)
+            assertFalse(actual)
+        }
+    }
+
+    //Testing validatePassword
+    @Test
+    fun `when pass valid password string then return false`() {
+        listOf(
+            "Passw0rd",
+            "StrongPassword123",
+            "NoSpaces1",
+            "6CharsA",
+            "LongPasswordWithNumbers12345"
+        ).forEach { password ->
+            val actual = AuthHelper.validatePassword(password)
+            assertTrue(actual)
+        }
+    }
+
+    @Test
+    fun `when pass invalid password string then return false`() {
+        listOf(
+            "Short1",    // Doesn't have at least 6 chars
+            "NoNumber",  // Doesn't have at least 1 number
+            " WithSpace", // Has whitespaces
+            " 123 456 ",  // Has whitespaces
+            "    ",       // Has only whitespaces
+            ""            // Empty string
+        ).forEach { password ->
+            val actual = AuthHelper.validatePassword(password)
+            assertFalse(actual)
+        }
+    }
+}
--- app/src/test/java/com/aleexalvz/cashwise/helper/CurrencyHelperKtTest.kt
+++ app/src/test/java/com/aleexalvz/cashwise/helper/CurrencyHelperKtTest.kt
@@ -8,13 +8,67 @@ class CurrencyHelperKtTest {
     @Test
     fun `when convert zero then return right currency value`() {
         //Arrange
-        val zero = 0.0
-        val expectedCurrency = "R$ 0,00"
+        val value = 0.0
+        val expected = "R$ 0,00"
 
         //Act
-        val actualCurrency = zero.toCurrencyString()
+        val actualValue = value.toCurrencyString()
 
         //Assert
-        assertEquals(expectedCurrency, actualCurrency)
+        assertEquals(expected, actualValue)
     }
+
+    @Test
+    fun `when convert value then return right value validating that is rounded to down`(){
+        //Arrange
+        val value = 15.159
+        val expected = "R$ 15,15"
+
+        //Act
+        val actualValue = value.toCurrencyString()
+
+        //Assert
+        assertEquals(expected, actualValue)
+    }
+
+    @Test
+    fun `when convert large value then return right value validating format`(){
+        //Arrange
+        val value = 151591591.15
+        val expected = "R$ 151.591.591,15"
+
+        //Act
+        val actualValue = value.toCurrencyString()
+
+        //Assert
+        assertEquals(expected, actualValue)
+    }
+
+    @Test
+    fun `when convert positive value then return right currency value`() {
+        //Arrange
+        val value = 1.57
+        val expected = "R$ 1,57"
+
+        //Act
+        val actualValue = value.toCurrencyString()
+
+        //Assert
+        assertEquals(expected, actualValue)
+    }
+
+    @Test
+    fun `when convert negative value then return right currency value with negative symbol before that`() {
+        //Arrange
+        val value = -1.57
+        val expected = "-R$ 1,57"
+
+        //Act
+        val actualValue = value.toCurrencyString()
+
+        //Assert
+        assertEquals(expected, actualValue)
+    }
+
+
 }
